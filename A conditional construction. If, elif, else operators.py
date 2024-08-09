b562a743e5815902c9738327417b7fa15a2c9702
SHISHKA-afk <ur.ilya@mail.ru>
2024-08-09 11:53:40 +0300

    homework

---
+++ A conditional construction. If, elif, else operators.py
@@ -0,0 +1,8 @@
+pack = {int(input('First value = ')), int(input('Second value = ')), int(input('Third value = '))}
+quantity = len(pack)
+if quantity == 1:
+    print(3)
+elif quantity == 2:
+    print(2)
+else:
+    print(0)
