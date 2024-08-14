549a261be7d0ebf26db47b647821b0116ef9f9a1
SHISHKA-afk <ur.ilya@mail.ru>
2024-08-14 14:40:42 +0300

    homework

---
+++ module_3_3.py
@@ -0,0 +1,13 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+print_params(b=25)
+print_params(c=[1, 2, 3])
+values_list = (True, 5, "Tool")
+values_dict = {'a': False, 'b': "5", 'c': 5}
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
