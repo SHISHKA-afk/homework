9854e65486b447b65bb8691b5139b1d4bd9b9ebc
SHISHKA-afk <ur.ilya@mail.ru>
2024-10-01 00:11:17 +0300

    homework

---
+++ module_3_hard.py
@@ -0,0 +1,39 @@
+def calculate_sum(massive):
+    total_sum = 0
+    def recursive_sum(element):
+
+        nonlocal total_sum
+
+        if isinstance(element, int):
+            total_sum += element
+
+        elif isinstance(element, str):
+            total_sum += len(element)
+
+        elif isinstance(element, (list, tuple)):
+            for i in element:
+                recursive_sum(i)
+
+        elif isinstance(element, dict):
+            for key, value in element.items():
+                recursive_sum(key)
+                recursive_sum(value)
+
+        elif isinstance(element, set):
+            for i in element:
+                recursive_sum(i)
+
+    recursive_sum(massive)
+
+    return total_sum
+
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_sum(data_structure)
+print(result)
